54b0271522d6dfda3b336b2d00c98d9867e44dd1
James Morris <jamesmorris2@gmail.com>
2023-06-29 13:11:54 -0400

    Print results with color

--- test/test.sh
+++ test/test.sh
@@ -8,9 +8,9 @@ shells='sh csh tcsh bash ksh fish zsh ash dash heirloom-sh'
 echo "Checking for shells..."
 for shell in $shells; do
     if command -v "$shell" >> /dev/null; then
-        echo "test passed: '$shell' found";
+        printf "\033[1;32mtest passed: '%s' found\n\033[0;0m" "$shell"
     else
-        echo "test failed: '$shell' not found";
+        printf "\033[1;31mtest failed: '%s' not found\n\033[0;0m" "$shell"
         exit 1
     fi
 done
