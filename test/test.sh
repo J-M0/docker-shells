001a7ee50f6e89bac6e21ff4002f43d5f1001ab5
James Morris <jamesmorris2@gmail.com>
2023-06-29 13:11:01 -0400

    Make script POSIX

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 # Bomb if anything fails.
 set -e
 
 # Check each shell.
-shells=(sh csh tcsh bash ksh fish zsh ash dash heirloom-sh)
+shells='sh csh tcsh bash ksh fish zsh ash dash heirloom-sh'
 
 echo "Checking for shells..."
-for shell in "${shells[@]}"; do
-    if which "$shell" >> /dev/null; then
+for shell in $shells; do
+    if command -v "$shell" >> /dev/null; then
         echo "test passed: '$shell' found";
     else
         echo "test failed: '$shell' not found";
