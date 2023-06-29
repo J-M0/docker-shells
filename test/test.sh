05afda55992b9ca4ea2472fa8e721551dd7be433
James Morris <jamesmorris2@gmail.com>
2023-06-29 13:09:39 -0400

    Get rid of function

--- test/test.sh
+++ test/test.sh
@@ -3,20 +3,14 @@
 set -e
 
 # Check each shell.
-function check_shell
-{
-    local shell_command="$1"
-    if which "$shell_command" >> /dev/null; then
-        echo "test passed: '$shell_command' found";
-    else
-        echo "test failed: '$shell_command' not found";
-        exit 1
-    fi
-}
-
 shells=(sh csh tcsh bash ksh fish zsh ash dash heirloom-sh)
 
 echo "Checking for shells..."
 for shell in "${shells[@]}"; do
-    check_shell "$shell"
+    if which "$shell" >> /dev/null; then
+        echo "test passed: '$shell' found";
+    else
+        echo "test failed: '$shell' not found";
+        exit 1
+    fi
 done
